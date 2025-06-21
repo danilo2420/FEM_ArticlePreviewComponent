18c3b1a509856141e5b3c5f580261d14fd206745
Danilo <danimoraga1997@gmail.com>
2025-06-21 15:37:11 +0200

    Working on switching between elements

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("Hey now brown cow");
+
+const lightIcon = document.getElementById('lightIcon');
+const darkIcon = document.getElementById('darkIcon');
+
+function main() {
+    console.log("Main is playing");
+    console.log(lightIcon);
+    console.log(darkIcon);
+
+    setEventListeners();
+}
+
+function setEventListeners() {
+    lightIcon.addEventListener('click', (event) => {
+        console.log("light icon was clicked");
+    });
+    
+    darkIcon.addEventListener('click', (event) => {
+        console.log("dark icon was clicked");
+    })
+}
+
+main();
