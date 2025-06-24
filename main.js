0217f7465eb9a40a15f5ef45403a247123bb6162
dani <danimoraga1997@gmail.com>
2025-06-24 16:56:04 +0200

    It works at least

--- main.js
+++ main.js
@@ -9,36 +9,28 @@ const popUp = document.getElementById('popUp');
 const iconImage = document.getElementById('iconImage');
 
 function main() {
-    console.log("Main is playing");
-    console.log(lightIcon);
-    console.log(darkIcon);
-
     setEventListeners();
 }
 
 function setEventListeners() {
+    // This icon is used to turn on the pop up when screen is small
+    // and to turn it on and off when the screen is medium or large
     lightIcon.addEventListener('click', (event) => {
-        if (screen.width < 650) {
-            bottomSection.style.display = 'none';
-            socialMediaSection.style.display = 'flex';
-        } else {
-            toggleLightIcon();
-        }
+        togglePopUp();
     });
-    
+
+    // This icon only appears when the screen is small
     darkIcon.addEventListener('click', (event) => {
-        if (screen.width < 650) {
-            bottomSection.style.display = 'flex';
-            socialMediaSection.style.display = 'none';
-        }
+        togglePopUp();
     })
 }
 
-let b = true;
+// This just toggles the style of the light icon when used for turning both on and off
+// let iconIsLight = true; // Initialized as light
 function toggleLightIcon() {
     lightIcon.classList.toggle('darkIconContainer');
 
-    if (b) {
+    if (isPopUpOn) {
         iconImage.src = 'images/icon-share-white.svg';
         popUp.style.display = 'flex';
     } else {
@@ -46,11 +38,36 @@ function toggleLightIcon() {
         popUp.style.display = 'none';
     }
 
-    b = !b;
+    //iconIsLight = !iconIsLight;
 }
 
 function resize() {
     // I'll have to do stuff here so that things don't get messed up
+}
+
+function togglePopUp() {
+    isPopUpOn = !isPopUpOn;
+    makePopupAppear(isPopUpOn);
+}
+
+var isPopUpOn = false;
+function makePopupAppear(b) {
+    if (b) { // Show pop up
+        if (screen.width < 650) { // Small screens
+            bottomSection.style.display = 'none';
+            socialMediaSection.style.display = 'flex';
+        } else { // Medium and large screens
+            toggleLightIcon(true);
+        }
+    } else { // Hide pop up
+        if (screen.width < 650) { // Small screens
+            bottomSection.style.display = 'flex';
+            socialMediaSection.style.display = 'none';
+        } else { // Medium and large screens
+            toggleLightIcon(false);
+        }
+    }
+    isPopUpOn = b;
 }
 
 main();
